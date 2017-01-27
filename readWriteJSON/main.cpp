37c4a633e5b06d71db2feb478f92a0be30a058f2
AnnaBuzian <buzyan2008@gmail.com>
2017-01-27 13:22:20 +0200

    Initial Commit

---
+++ readWriteJSON/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  readWriteJSON
+//
+//  Created by Anna Buzian on 27.01.17.
+//  Copyright © 2017 Anna Buzian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
